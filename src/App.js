1aaf8a4ab32d9ca7a43fd171214a905edc191656
kobrak1 <burakkarhan657@gmail.com>
2023-10-15 22:04:59 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+
+function App() {
+  const [notes, setNotes] = useState([])
+  const [currentNote, setCurrentNote] = useState('')
+  const [noteId, setNoteId] = useState(1)
+
+  const addNote = (note) => {
+    if(note) {
+      setNotes([...notes, {id: noteId, content: note }])
+      setNoteId(noteId + 1)
+      setCurrentNote('')
+    } else {
+      setNoteId(1)
+      console.log('Add note')
+      alert('Add note')
+    }
+  }
+
+  const deleteNote = (id) => {
+      const updatedNotes = notes.filter(note => note.id !== id);
+      setNotes(updatedNotes);
+  }
+
+  return (
+    <div className='App'>
+      <form action="#">
+        <input 
+        type="text"
+        value={currentNote}
+        onChange={e => setCurrentNote(e.target.value)}
+        placeholder='Add note...' />
+        <input 
+        type="submit"
+        value={'Add'}
+        onClick={() => addNote(currentNote)} />
+      </form>
+      <div>
+        <ul>
+          {notes.map((e, index) => <li 
+            key={index} 
+            style={{listStyleType:'none'}}>
+              <div>
+                <span>
+                  {e.id}-{e.content}
+                </span>
+                <button onClick={() => deleteNote(e.id)}>Delete</button>
+              </div>
+            </li> )}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Note = ({note}) => {
+  return (
+    <div>
+      <p> {note} </p>
+    </div>
+  )
+}
+
+export default Note
