f14b3ab5831672c04b6984f6c93836553fa4d35e
kobrak1 <burakkarhan657@gmail.com>
2023-10-20 20:43:32 +0300

    some styling added

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -2,12 +2,20 @@ import React from "react";
 import './Note.css'
 
 const Note = ({ notes, deleteNote }) => {
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString();
   return (
     <ul className="note-list">
       {notes.map((e, index) => (
         <li className="note" key={index} style={{ listStyleType: "none" }}>
           <div>
-            <span>{e.content}</span>
+            <span>
+              <div className="title">
+                <p><b>Title</b></p>
+                <p>{formattedDate}</p>
+              </div>
+              <p>{e.content}</p>
+            </span>
             <button onClick={() => deleteNote(e.id)}>Delete</button>
           </div>
         </li>
