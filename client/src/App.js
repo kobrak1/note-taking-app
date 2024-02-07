5c2dcb7006e4b94684ac6aea631d8f2cfb6489b5
kobrak1 <burakkarhan657@gmail.com>
2024-02-07 16:43:01 +0300

    feat: data fetch from a json server feature is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
 
 function App() {
-  const notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true,
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false,
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true,
-    },
-  ];
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  console.log(isLoading);
+  console.log('burak karhan');
+
+  useEffect(() => {
+    fetch("http://localhost:3001/notes")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error while fetching data:", error);
+        setIsLoading(false)
+      });
+  }, []);
+  
+  console.log(isLoading);
+  console.log(data);
 
   return (
     <>
       <Header />
-      <Content notes={notes} />
+      {isLoading ? <p>lading...</p> : <Content data={data} />}
       <Footer />
     </>
   );
