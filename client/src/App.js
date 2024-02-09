5af42beecbcac5a71694ec05c35991b2fc9f3b65
kobrak1 <burakkarhan657@gmail.com>
2024-02-09 20:00:04 +0300

    chore: update

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,14 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
+import getAll from "./services/services.jsx";
 
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:3001/notes")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok!");
-        }
-        return response.json();
-      })
+    getAll()
       .then((data) => {
         setData(data);
         setIsLoading(false);
