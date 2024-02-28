c3272a8c41fc7bc71965a485d57d5f5e3b9c8692
kobrak1 <burakkarhan657@gmail.com>
2024-02-28 16:57:46 +0300

    update: dotenv library imported with the config() method

--- api/models/note.js
+++ api/models/note.js
@@ -1,30 +1,32 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const url = process.env.MONGO_URI
+const url = process.env.MONGO_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-console.log('connecting to', url);
+console.log("connecting to", url);
 
-mongoose.connect(url)
-.then(result => {
-  console.log('Connected to MongoDB')
-})
-.catch(error => {
-  console.log('error connecting to mongodb:', error.message)
-})
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to mongodb:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
