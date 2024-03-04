56bcbc689e1e75e378d0749981730067dc519648
kobrak1 <burakkarhan657@gmail.com>
2024-03-04 15:24:38 +0300

    uibuild

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.get("/api/notes", (request, response) => {
 // GET SPECIFIED DATA
 app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
-    .then( note => {
+    .then((note) => {
       if (note) {
         response.json(note);
       } else {
@@ -37,30 +37,29 @@ app.get("/api/notes/:id", (request, response, next) => {
 // DELETE METHOD
 app.delete("/api/notes/:id", (request, response) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
 // PUT METHOD
 app.put("/api/notes/:id", (request, response) => {
-  const body = request.body
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
-    .then(updatedNote => {
-      response.json(updatedNote)
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 // POST METHOD
-app.post("/api/notes/", (request, response) => {
+app.post("/api/notes/", (request, response, next) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({
@@ -73,31 +72,34 @@ app.post("/api/notes/", (request, response) => {
     important: Boolean(body.important) || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // ERROR HANDLERS
 const unknownEndPoint = (request, response) => {
-  response.status(404).end()
-}
+  response.status(404).end();
+};
 // handler of requests with unknown endpoint
-app.use(unknownEndPoint) 
+app.use(unknownEndPoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    response.status(404).send({error: 'malforatted id'})
+  if (error.name === "CastError") {
+    return response.status(404).send({ error: "malforatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 // handler of requests with unknown id
-app.use(errorHandler) 
-
-
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
