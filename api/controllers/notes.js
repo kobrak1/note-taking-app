9b37212029840ddccbb1cbaef79bafce4ee621c8
kobrak1 <burakkarhan657@gmail.com>
2024-03-08 13:36:31 +0300

    add: notes.js is added to handle routes. It will be the controller of 'notes'

---
+++ api/controllers/notes.js
@@ -0,0 +1,71 @@
+﻿const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+// GET ALL
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
+})
+
+
+// GET SPECIFIED DATA
+notesRouter.get('/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+// DELETE METHOD
+notesRouter.delete('/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+// PUT METHOD
+notesRouter.put('/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+// POST METHOD
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: Boolean(body.important) || false,
+  })
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = notesRouter
