1f7b160452cfb6890bba9c27ecec250ebc11666a
kobrak1 <burakkarhan657@gmail.com>
2024-03-24 02:01:14 +0300

    add: users and notes api populated to show detailed information about who created the note and which notes are created by the user.

--- api/controllers/notes.js
+++ api/controllers/notes.js
@@ -1,10 +1,11 @@
 ﻿const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 // GET ALL
 notesRouter.get('/', async (request, response, next) => {
   try {
-    const notes = await Note.find({})
+    const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
     response.status(200).json(notes)
   } catch (exception) {
     next(exception)
@@ -14,7 +15,7 @@ notesRouter.get('/', async (request, response, next) => {
 // GET SPECIFIED DATA
 notesRouter.get('/:id', async (request, response, next) => {
   try {
-    const note = await Note.findById(request.params.id)
+    const note = await Note.findById(request.params.id).populate('user', { username: 1, name: 1 })
     if (note) {
       response.json(note)
     } else {
@@ -54,6 +55,10 @@ notesRouter.put('/:id', async (request, response, next) => {
 // POST METHOD
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const user = await User.findById(body.userId)
+  console.log('This is the body:', body)
+  console.log('This is the user:', user)
+
   if (!body.content) {
     return response.status(400).json({
       error: 'content missing',
@@ -63,9 +68,13 @@ notesRouter.post('/', async (request, response, next) => {
   const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
+    user: user.id
   })
   try {
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.status(201).json(savedNote)
   } catch (exception) {
     next(exception)
--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,6 +2,19 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// get all users
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('notes', { content: 1, important: 1 })
+  response.status(200).json(users)
+})
+
+// get a specific user
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id).populate('notes', { content: 1, important: 1 })
+  response.status(200).json(user)
+} )
+
+// post a new user
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
