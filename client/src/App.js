5d58b44ce37267f3b446f52077a16ded2cc68049
kobrak1 <burakkarhan657@gmail.com>
2024-03-31 22:55:59 +0300

    update: App.js is updated to handle the user login process

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
-import noteService from "./services/services.jsx";
+import noteService from "./services/services.js";
+import loginService from "./services/login.js"
+import Login from "./components/Login/Login.jsx";
 
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ user, setUser ] = useState(null);
 
   useEffect(() => {
     noteService
@@ -21,12 +26,33 @@ function App() {
       });
   }, []);
 
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const user = await loginService.login({ 
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log('error:', error.message);
+    }
+  }
+  
   if(!data) {
     return null
   }
 
   return (
     <>
+      <Login 
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      handleLogin={handleLogin} />
       <Header />
       {isLoading ? <p>loading...</p> : <Content data={data} />}
       <Footer />
