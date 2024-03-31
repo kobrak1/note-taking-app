2fc86c2dca9570b5e1a41727c34dc565a6cd3ad9
kobrak1 <burakkarhan657@gmail.com>
2024-03-31 22:39:33 +0300

    update: services.js is updated

---
+++ client/src/services/services.js
@@ -0,0 +1,47 @@
+const baseUrls = '/api/notes'; // relative url
+const baseUrl = 'http://localhost:3001/api/notes'
+
+const getAll = async () => {
+  const request = await fetch(baseUrl);
+  if (!request.ok) {
+    throw new Error("Error while fetching data");
+  }
+  console.log('data fetched successfully');
+  return request.json();
+};
+
+const post = (notes, noteObject,setNotes) => {
+  fetch(baseUrl, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(noteObject.content && noteObject),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error while posting data!");
+      }
+      return response.json();
+    })
+    .then(noteObject.content && setNotes([...notes, noteObject]))
+    .then((data) => console.log("Response data:", data))
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+const update = async (id, newObject) => {
+  const request = await fetch(`${baseUrl}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newObject),
+  });
+
+  if (!request.ok) {
+    throw new Error("Error while updating the data");
+  }
+  return request.json();
+};
+
+export default { getAll, update, post };
