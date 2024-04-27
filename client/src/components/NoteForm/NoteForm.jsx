0356c61d536507318d245ee9fc71b5c7e8d97cb4
kobrak1 <burakkarhan657@gmail.com>
2024-04-27 16:28:41 +0300

    update: App.js and index.js are transformed to App.jsx and main.jsx since the vite installed into the project. Note.jsx and NoteForm.jsx components are transformed to keep up with the eslint rules such as removal of a semicolon and extra spaces

--- client/src/components/NoteForm/NoteForm.jsx
+++ client/src/components/NoteForm/NoteForm.jsx
@@ -1,31 +1,31 @@
 import { useState } from 'react'
 
 const NoteForm = ({ createNote }) => {
-  const [newNote, setNewNote] = useState('');
+  const [newNote, setNewNote] = useState('')
 
-    // add new note
-    const addNote = (e) => {
-      e.preventDefault();
-      createNote({
-          content: newNote,
-          important: true,
-      }) 
-      setNewNote('')
-    }
-
-    return (
-      <div>
-        <h2>Create a new note</h2>
-  
-        <form onSubmit={addNote}>
-          <input
-            value={newNote}
-            onChange={e => setNewNote(e.target.value)}
-          />
-          <button type="submit">save</button>
-        </form>
-      </div>
-    )
+  // add new note
+  const addNote = (e) => {
+    e.preventDefault()
+    createNote({
+      content: newNote,
+      important: true,
+    })
+    setNewNote('')
   }
 
-  export default NoteForm
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={e => setNewNote(e.target.value)}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default NoteForm
