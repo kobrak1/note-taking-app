e7ef17be28054d78e3b21cb159a0a65a9cf20382
kobrak1 <burakkarhan657@gmail.com>
2024-04-30 10:53:54 +0300

    update: LoginForm s form fields username and password is given data-testid values to identify them while testing

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -13,6 +13,7 @@ const LoginForm = ({
       <div>
         username
         <input
+          data-testid='username'
           type="text"
           value={username}
           name="Username"
@@ -21,6 +22,7 @@ const LoginForm = ({
       <div>
         password
         <input
+          data-testid='password'
           type="password"
           value={password}
           name="Password"
