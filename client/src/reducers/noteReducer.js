710efb85a63fcf9dd8416a7500a7ae08b5e72e51
kobrak1 <burakkarhan657@gmail.com>
2024-05-28 12:13:12 +0300

    add: noteReducer and its test is added

---
+++ client/src/reducers/noteReducer.js
@@ -0,0 +1,26 @@
+const noteReducer = (state=['bura karhan'], isAction) => {
+  return state
+}
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+export default noteReducer
