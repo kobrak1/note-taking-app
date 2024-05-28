e40395ff9450c8ffc269d33eb9b2a05b86269468
kobrak1 <burakkarhan657@gmail.com>
2024-05-28 12:14:13 +0300

    main.jsx wrapped with provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import App from './App'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
