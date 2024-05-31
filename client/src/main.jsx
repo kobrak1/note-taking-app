d39cd0f27c810bdb5d7a9262ae1d4abc080f67d4
kobrak1 <burakkarhan657@gmail.com>
2024-05-31 16:45:26 +0300

    update: store is updated by combining both noteReducer and filterReducer with combineReducers function of redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,21 @@
 import ReactDOM from 'react-dom/client'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import noteReducer, { createNote } from './reducers/noteReducer'
+import filterReducer, { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+    notes: noteReducer,
+    filterNote: filterReducer
+})
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('Ananın amı amına koyduğumun çocuğu'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
