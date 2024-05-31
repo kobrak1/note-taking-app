fe2003e4c57d192fc2f0df34282f6e6e3547871d
kobrak1 <burakkarhan657@gmail.com>
2024-05-31 16:42:32 +0300

    update: constructed the App.jsx file with NewNote, VisibilityFilter and Notes components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import NewNote from "./components/NewNote/NewNote"
 import Notes from "./components/Notes/Notes"
+import VisibilityFilter from "./components/VisibilityFilter/VisibilityFilter"
 
 const App = () => {
     return (
         <div>
             <NewNote />
+            <VisibilityFilter />
             <Notes />
         </div>
     )
