8018d2d69cf641d22a0394fd68b6affdd1cf4ea1
kobrak1 <burakkarhan657@gmail.com>
2024-06-01 18:26:44 +0300

    update: noteReducer.test.js file is updated after useing createSlice

--- client/src/reducers/noteReducer.test.js
+++ client/src/reducers/noteReducer.test.js
@@ -5,11 +5,9 @@ describe('noteReducer', () => {
   test('returns new state with action NEW_NOTE', () => {
     const state = []
     const action = {
-      type: 'NEW_NOTE',
+      type: 'notes/createNote',
       payload: {
         content: 'the app state is in redux store',
-        important: true,
-        id: 1
       }
     }
 
