5f18fbfa74aa154d89e81acc34978e8c52c3edb8
kobrak1 <burakkarhan657@gmail.com>
2024-06-02 15:21:26 +0300

    update: reducers are updated by changing their initial states

--- client/src/reducers/noteReducer.js
+++ client/src/reducers/noteReducer.js
@@ -1,24 +1,14 @@
-import uuid4 from 'uuid4'
 import { createSlice } from '@reduxjs/toolkit'
-
-const initialState = [
-    { content: 'reducer defines how redux store works', important: true, id: 1},
-    { content: 'state of store can contain any data', important: false, id: 2}
-  ]
 
 // slice reducers
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     // add new notes
     createNote(state, action) {
       const content = action.payload
-      state.push({
-        content,
-        important: false,
-        id: uuid4()
-      })
+      state.push(content)
     },
     // toggle between 'important' and 'not imporant' buttons
     toggleImportanceOf(state, action) {
