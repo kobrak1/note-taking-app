89bdd167e742f53cf427353d45e2e8b831a9a95d
kobrak1 <burakkarhan657@gmail.com>
2024-06-02 15:20:13 +0300

    update.

--- client/src/components/NewNote/NewNote.jsx
+++ client/src/components/NewNote/NewNote.jsx
@@ -1,18 +1,29 @@
 import { useDispatch } from 'react-redux'
 import { createNote } from '../../reducers/noteReducer'
+import uuid4 from 'uuid4'
 
 const NewNote = () => {
     const dispatch = useDispatch()
 
-    const addNote = (event) => {
+    // helper function
+    const asObject = (content) => {
+        return {
+            content,
+            important: false,
+            id: uuid4()
+        }
+    }
+    
+    // handle submit method
+    const handleSubmit = (event) => {
         event.preventDefault()
         const content = event.target.note.value
         event.target.note.value = ''
-        dispatch(createNote(content))
+        dispatch(createNote(asObject(content)))
     } 
 
     return (
-        <form onSubmit={addNote}>
+        <form onSubmit={handleSubmit}>
             <input name="note" />
             <button type="submit">add</button>
         </form>
