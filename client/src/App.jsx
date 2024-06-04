fe711fd4075d485ca448884fc7a380b817e30d10
kobrak1 <burakkarhan657@gmail.com>
2024-06-04 13:56:40 +0300

    update: effect hook in the App.jsx is updated by dispatching initializeNotes() function on every render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react"
 import NewNote from "./components/NewNote/NewNote"
 import Notes from "./components/Notes/Notes"
 import VisibilityFilter from "./components/VisibilityFilter/VisibilityFilter"
+import noteService from "./services/noteService"
+import { initializeNotes, setNotes } from "./reducers/noteReducer"
+import { useDispatch } from "react-redux"
 
 const App = () => {
+    const dispatch = useDispatch()
+    // get all the data whenever the App component is rendered
+    useEffect(() => {
+        dispatch(initializeNotes())
+    }, [])
+
     return (
         <div>
             <NewNote />
