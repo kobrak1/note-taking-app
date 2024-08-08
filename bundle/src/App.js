929435f20d10f1d6a4cf5ff44990274e39c315b7
kobrak1 <aburakkarhan@gmail.com>
2024-08-08 11:59:36 +0300

    add: sample app logic and styling

--- bundle/src/App.js
+++ bundle/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react' // we need this now also in component files
+import React, { useState, useEffect } from 'react'
+import { useNotes } from './hooks/useNotes'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+  console.log('notes', notes)
+  console.log('BACK URL:', BACKEND_URL)
+
   return (
-    <div>
-      hello webpack
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>press</button>
+
+      <div>{notes.length} notes on server {BACKEND_URL}</div>
     </div>
   )
 }
