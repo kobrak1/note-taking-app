a20c40a779648b4e5ca42ef98b9c676cfeb5e226
kobrak1 <burakkarhan657@gmail.com>
2024-10-06 23:42:59 +0300

    chore: App.jsx routing was redesigned with react-router-dom v6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,42 @@
 import React, { lazy, Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from "./pages/Layout.jsx"
+import FallBackUI from "./components/FallBackUI.jsx"
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 // lazy loaded components
-import PrivateRoutes from './utils/PrivateRoutes'
-import Login from './components/Login'
-import HomePage from './pages/HomePage'
-import NoteFormPage from './pages/NoteFormPage'
+const HomePage = lazy(() => import(/* webpackChunkName: "HomePage" */ "./pages/HomePage.jsx"))
+const ListPage = lazy(() => import(/* webpackChunkName: "ListPage" */ "./pages/ListPage.jsx"))
+const SinglePage = lazy(() => import(/* webpackChunkName: "SinglePage" */ "./pages/SinglePage.jsx"))
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path:"/",
+          element: <HomePage />
+        },
+        {
+          path: "/notes",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        }
+      ]
+    }
+  ])
+
   return (
-    <div>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/create-new' element={<NoteFormPage />} />
-        </Route>
-      </Routes>
-    </div>
+    <ErrorBoundary>
+      <Suspense fallback={ <FallBackUI /> }>
+        <RouterProvider router={router} />
+      </Suspense>
+    </ErrorBoundary>
   )
 }
 
