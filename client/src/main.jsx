77b8f2b198f5d8d0cf0f76dcf0222db27578ddb9
kobrak1 <burakkarhan657@gmail.com>
2024-10-06 23:44:47 +0300

    updated: unnecessary react Query provider wrappers have been removed since the project is not utilized from react Query anymore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import App from './App.jsx'
-import './index.css'
-
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
