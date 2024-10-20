0d7cd7d07c1fea88435e1b87938f2c6172863398
kobrak1 <burakkarhan657@gmail.com>
2024-10-20 21:29:45 +0300

    modified: modified

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,11 @@ export default {
         primary: '#f8fafc',    // slate-50
         secondary: '#cbd5e1',  // slate-300
         tertiary: '#64748b',   // slate-500
+
+        // button colors
+        btnPrimary: '#60a5fa',
+        btnSecondary: '#3b82f6',
+        btnTertiary:'#2563eb',
       },
     },
   },
